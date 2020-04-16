1cc69ac86f045fb8972d3bebcfd59a225962a71f
Dennis Dinh Hung <dennisdinhhung@gmail.com>
2020-04-16 15:17:18 +0700

    Moddified hom.php

--- home.php
+++ home.php
@@ -3,11 +3,7 @@
 <head>
 	<title></title>
 </head>
-<<<<<<< HEAD
 <link rel="stylesheet" href="home.css">
-=======
-<link rel="stylesheet" href="Assignment2 style.css">
->>>>>>> 37938a1ec1efec3bf7909be5defc063e6256fdd7
 <link rel="icon" href="image/shoes/back.png" type="image/gif" sizes="16x16">
 <body class="webpage" >
 	<div>
@@ -21,11 +17,7 @@
 			<nav class="content">
 				<p class="bcontent"> <span style="font-size: 30px,">POWERED BY THE PAST</span> <br>
 				Taking design from the 90’s, OZWEEGO <br> has defined midsole lines, a translucent <br> support tube, and adiPRENE technology.</p>
-<<<<<<< HEAD
 				<a href="listpage1.php" class="ShopN">SHOP NOW</a>
-=======
-				<a href="Assigment 2 listpage1.php" class="ShopN">SHOP NOW</a>
->>>>>>> 37938a1ec1efec3bf7909be5defc063e6256fdd7
 			</nav>
 		</div>
 	</div>
@@ -35,8 +27,4 @@
 		</div>
 	</footer>
 </body>
-<<<<<<< HEAD
 </html>
-=======
-</html>
->>>>>>> 37938a1ec1efec3bf7909be5defc063e6256fdd7
